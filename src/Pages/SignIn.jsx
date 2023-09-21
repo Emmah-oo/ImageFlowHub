49d865e263c5935730270106e543dd32b9d4fd9a
Emmah-oo <emmaochonogor715@gmail.com>
2023-09-21 14:38:25 +0100

    feat: implemented authorization and authentication

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,22 +1,51 @@
-import { Link } from "react-router-dom";
-
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebase";
 const SignIn = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [error, setError] = useState("");
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate("/");
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
   return (
     <div className="min-h-[80vh] w-[100%]">
       <form className="w-[80%] md:w-[60%] lg:w-[50%] m-auto flex min-h-[80vh] my-5 gap-y-10 flex-col items-center justify-center shadow-sm rounded-md">
         <input
           type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Email"
           className="border px-4 py-3 w-[80%] outline-none rounded-md"
         />
         <input
           type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
           className="border px-4 py-3 w-[80%] outline-none rounded-md"
         />
         <button
           type="submit"
           className="bg-blue-600 hover:bg-blue-500 text-white px-6 py-3 rounded-md transition-all border w-[70%]"
+          onClick={onLogin}
         >
           Login
         </button>
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,26 +1,60 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebase";
+
 const SignUp = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/signin");
+      })
+      .catch((error) => {
+        // An error happened.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
   return (
     <div className="min-h-[80vh] w-[100%]">
       <form className="w-[80%] md:w-[60%] lg:w-[50%] my-5 m-auto flex min-h-[80vh] gap-y-10 flex-col items-center justify-center shadow-sm rounded-md">
         <input
           type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+          className="border px-4 py-3 w-[80%] outline-none rounded-md"
+        />
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Email"
           className="border px-4 py-3 w-[80%] outline-none rounded-md"
         />
         <input
           type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="Create Password"
-          className="border px-4 py-3 w-[80%] outline-none rounded-md"
-        />
-        <input
-          type="password"
-          placeholder="Confirm Password"
           className="border px-4 py-3 w-[80%] outline-none rounded-md"
         />
         <button
           type="submit"
           className="bg-blue-600 hover:bg-blue-500 text-white px-6 py-3 rounded-md transition-all border w-[70%]"
+          onClick={onSubmit}
         >
           Signup
         </button>
